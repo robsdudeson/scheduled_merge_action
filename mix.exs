8d9e386b54b6421b3e95cda34f7be2b166908da0
Robby Thompson <robsdudeson@gmail.com>
2022-09-27 16:40:53 -0400

    update mix version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ScheduledMergeAction.MixProject do
   def project do
     [
       app: :scheduled_merge_action,
-      version: "0.1.0",
+      version: "0.0.1",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps()
