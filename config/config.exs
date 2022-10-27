16deb2eed5b4fca753f1ce036118a20c6c1e8356
Ross Brandes <ross@testdouble.com>
2022-10-27 12:51:55 -0400

    Add config into action
    So that maybe it actually can be read by the scheduled_merge library?

---
+++ config/config.exs
@@ -0,0 +1,18 @@
+import Config
+
+config :scheduled_merge,
+  github: [
+    repo: System.get_env("GITHUB_REPOSITORY"),
+    api_url: System.get_env("GITHUB_API_URL"),
+    api_token: System.get_env("GITHUB_TOKEN")
+  ],
+  labels: [
+    default_error_label: %{
+      name: "merge-failed",
+      color: "FF0000",
+      description: "indicates when scheduled merge does not work"
+    },
+    default_merge_label: %{
+      color: "00FF00"
+    }
+  ]
